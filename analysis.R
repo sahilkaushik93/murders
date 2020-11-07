a7c1cba95c8bbc3fd64b771e3214fd788d258179
sahilkaushik93 <kaushiksahil1412@gmail.com>
2020-11-07 20:25:49 +0530

    Us murders

---
+++ analysis.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+load("rda/murders.rda")
+
+murders %>% mutate(abb= reorder(abb,rate)) %>% 
+  ggplot(aes(abb,rate)) +
+  geom_bar(width = 0.5,stat = "identity",color="Yellow")+
+  coord_flip()
+
+ggsave("figs/barplot.png")
+
+
+
+
